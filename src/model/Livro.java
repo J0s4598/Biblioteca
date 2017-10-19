18163d64538664767c54105d6a985085d3815b49
ra21708452 <ra21708452@AN01201615077105.dinfor.uniceub>
2017-10-19 20:50:40 -0200

    Commit Iniçial

---
+++ src/model/Livro.java
@@ -0,0 +1,55 @@
+package model;
+
+public class Livro {
+	private String titulo;
+	private String autor;
+	private String editora;
+	private String anoLancamento;
+	private String ISBM;
+	
+	//Metodo Construtor
+	public Livro(String titulo, String autor, String editora, String anoLancamento, String iSBM) {
+		super();
+		this.titulo = titulo;
+		this.autor = autor;
+		this.editora = editora;
+		this.anoLancamento = anoLancamento;
+		ISBM = iSBM;
+	}
+
+	//Get e Set
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public String getEditora() {
+		return editora;
+	}
+	public void setEditora(String editora) {
+		this.editora = editora;
+	}
+	public String getAnoLancamento() {
+		return anoLancamento;
+	}
+	public void setAnoLancamento(String anoLancamento) {
+		this.anoLancamento = anoLancamento;
+	}
+	public String getISBM() {
+		return ISBM;
+	}
+	public void setISBM(String iSBM) {
+		ISBM = iSBM;
+	}
+
+	
+	
+
+}
